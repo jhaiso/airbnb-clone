879a81ab624c0118598eaaa0a71af081597e35c7
jeffreyhaisong <jeffrey.song.27@dartmouth.edu>
2024-07-24 22:13:40 -0400

    completed reservation logic & backend

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -4,9 +4,8 @@ import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
-import { SafeListing, SafeUser } from "@/app/types";
+import { SafeListing, SafeUser, SafeReservation } from "@/app/types";
 
-import { Reservation } from "@prisma/client";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
@@ -24,7 +23,7 @@ const initialDateRange = {
 }
 
 interface ListingClientProps {
-  reservations?: Reservation[];
+  reservations?: SafeReservation[];
   listing: SafeListing & {
     user: SafeUser
   };
